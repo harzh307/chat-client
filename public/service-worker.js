9ef017aa258afae2a79b79ab5b5cd14820980deb
harzh307 <harzh.patel@gmail.com>
2024-09-10 21:25:36 +0530

    commit code PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,19 +13,21 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/static/js/bundle.js',
+        // Add other assets you want to cache
+      ]);
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
